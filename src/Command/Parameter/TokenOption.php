668176416a625811dc6dda21fb6212d3442d3cbd
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2014-07-28 07:12:46 +0200

    commander update

---
+++ src/Command/Parameter/TokenOption.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the Ghostscript package
+ *
+ * @author Daniel Schröder <daniel.schroeder@gravitymedia.de>
+ */
+
+namespace GravityMedia\Ghostscript\Command\Parameter;
+
+use GravityMedia\Commander\Parameter\AbstractOption;
+
+/**
+ * The token option object
+ *
+ * @package GravityMedia\Ghostscript\Command\Parameter
+ */
+class TokenOption extends AbstractOption
+{
+    /**
+     * Prefix
+     */
+    const PREFIX = '-d';
+
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param null|string|\GravityMedia\Commander\Parameter\Argument $argument
+     */
+    public function __construct($name, $argument = null)
+    {
+        parent::__construct($name, $argument);
+        $this->setDelimiter('=');
+    }
+
+    /**
+     * Get prefix
+     *
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return self::PREFIX;
+    }
+}
