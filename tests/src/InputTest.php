d91ee9ecb38c1c637bb8cf71d0eaf718c5a2310f
Daniel Schröder <daniel.schroeder@evolver.de>
2016-08-17 17:29:32 +0200

    Added input test

---
+++ tests/src/InputTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the Ghostscript test package
+ *
+ * @author Daniel Schröder <daniel.schroeder@gravitymedia.de>
+ */
+
+namespace GravityMedia\GhostscriptTest;
+
+use GravityMedia\Ghostscript\Input;
+
+/**
+ * The input test class
+ *
+ * @package GravityMedia\GhostscriptTest
+ *
+ * @covers  \GravityMedia\Ghostscript\Input
+ */
+class InputTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefaultInput()
+    {
+        $input = new Input();
+
+        $this->assertNull($input->getProcessInput());
+        $this->assertNull($input->getPostScriptCode());
+        $this->assertInternalType('array', $input->getFiles());
+        $this->assertCount(0, $input->getFiles());
+    }
+
+    public function testInput()
+    {
+        $input = new Input();
+        $input->setProcessInput('input');
+        $input->setPostScriptCode('.setpdfwrite');
+        $input->addFile('/path/to/output/file.pdf');
+        $input->setFiles(['/path/to/output/file.pdf']);
+
+        $this->assertSame('input', $input->getProcessInput());
+        $this->assertSame('.setpdfwrite', $input->getPostScriptCode());
+        $this->assertInternalType('array', $input->getFiles());
+        $this->assertCount(1, $input->getFiles());
+    }
+}
